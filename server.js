2529042399ad9fce9107af893ceb21346dc09535
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-10-31 21:44:19 -0400

    Added employee tracker prompts

--- server.js
+++ server.js
@@ -6,3 +6,35 @@ db.connect(err => {
     console.log('Connected to Database');
     employee_tracker();
 });
+
+let employee_tracker = function () {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'prompt',
+        message: 'Select from the following.',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Log Out']
+    }]).then((answers) => {
+        if (answers.prompt === 'View all departments') {
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing all departments: ");
+                console.table(result);
+                employee_tracker();
+            });
+        } else if (answers.prompt === 'View all roles') {
+            db.query(`SELECT * FROM role`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing all roles: ");
+                console.table(result);
+                employee_tracker();
+            });
+        } else if (answers.prompt === 'View all employees') {
+            db.query(`SELECT * FROM employee`, (err, result) => {
+                if (err) throw err;
+                console.log("Viewing all employees: ");
+                console.table(result);
+                employee_tracker();
+            });
+        }
+    })
+}
