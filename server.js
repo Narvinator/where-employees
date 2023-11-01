77da78d45e41eb5862db3c555951103fe91a47e0
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-10-31 21:15:07 -0400

    Database access updated in script files

---
+++ server.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to Database');
+    employee_tracker();
+});
